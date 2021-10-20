40753b9d161fda34dd3e6a757da77863dbd29371
Arhum Shahzad <82200170+Arhum2@users.noreply.github.com>
2021-10-20 00:55:23 -0400

    added features to sell page

--- app.py
+++ app.py
@@ -201,17 +201,34 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    if request.method =='GET':
-        return render_template('sell.html')
+    if request.method == 'GET':
+        return render_template('buy.html')
 
     symbol = request.form.get('symbol')
+
     result = lookup(request.form.get('symbol'))
-    
     if not result:
         return render_template('sell.html', invalid=True, symbol=symbol)
 
+    username = session['user_id']
+    shares = int(request.form.get('shares'))
+    result = lookup(symbol)
+    price = result['price']
+    name = result['name']
 
-    return render_template('sell.html')
+    db.execute("SELECT SUM(shares) as totalShares FROM buy WHERE ")
+
+    cash = db.execute('SELECT cash FROM users WHERE id = ?', username)[0]['cash']
+    bill = cash + shares*price
+
+    now = datetime.now()
+
+    db.execute('UPDATE users SET cash = ? WHERE id = ?', bill, username)
+    db.execute('INSERT into buy (username, symbol, shares, price, time) VALUES (?, ?, ?, ?, ?)', username, symbol, shares, price, now)
+
+    else:
+        rows = db.execute("SELECT symbol FROM buy WHERE id = ? GROUP BY symbol HAVING SUM(shares) > 0 ", username)
+        return render_template("sell.html", symbols=[ row["symbol"] for row in rows])
 
 
 def errorhandler(e):
